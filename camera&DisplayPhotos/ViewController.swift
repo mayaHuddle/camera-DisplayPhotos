78a7a14b357d93c686b31e0969f4c44391e93d1c
mayaHuddle <maya.huddle@gmail.com>
2021-06-29 17:22:49 -0400

    no changes

--- camera&DisplayPhotos/ViewController.swift
+++ camera&DisplayPhotos/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
-
-
+    
+    @IBAction func choosePhotoLibraryTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func takeSelfieTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+            
+    @IBOutlet weak var displayImage: UIImageView!
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]){
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            displayImage.image = selectedImage
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
 }
 
